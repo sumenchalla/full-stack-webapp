9ca21cd642cd7ccd2bfbdc466e61abdf3e20f26e
SUMEN <sumenchalla51643@gmail.com>
2025-11-18 10:09:11 +0530

    login page done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import UserLayout from "./components/Layout/UserLayout"
 import AdminLayout from "./components/Layout/AdminLayout"
 import Home from './pages/Home'
 import { Toaster } from 'sonner'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 function App() {
 
@@ -15,6 +17,8 @@ function App() {
         <Routes>
           <Route path="/" element={<UserLayout />}>
             <Route index element={<Home />}></Route>
+            <Route path='login'  element={<Login />}></Route>
+            <Route path='register'  element={<Register />}></Route>
           </Route>
           
           <Route>{/* Admin Layout */}</Route>
